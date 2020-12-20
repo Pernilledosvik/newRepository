89f2c224adbc54b97c52b57c3720a4dcdaabcf4e
pernilledosvik <pernilledosvik@gmail.com>
2020-12-20 22:45:02 +0100

    first upload

---
+++ src/Components/NavBar.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './NavBar.css';
+
+function NavBar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true); 
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className='navbar-logo'>
+                        I like to work out <i class='fas fa-dumbbell' />
+                    </Link>
+                    <div className='.menu-icon' onClick={handleClick}>
+                        <i className={click ? ' fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click? 'nav-menu active' : 'nav-menu'}> 
+                        <li className='nav-item'>
+                            <Link to='/trening' className='nav-links' onClick={closeMobileMenu}>
+                                Trening
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                Sign up
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonSyle='btn--outline'>SIGN UP</Button>}
+                </div>
+            
+            </nav>                          
+        </>
+    );
+}
+
+export default NavBar;
