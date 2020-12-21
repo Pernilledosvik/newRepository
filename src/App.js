2150261e340090550d9c3ac460ba78906fa5f67f
pernilledosvik <pernilledosvik@gmail.com>
2020-12-21 11:20:32 +0100

    upload 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+
 import React from 'react';
-import NavBar from "./Components/NavBar";
+import Navbar from './components/NavBar';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
-  <>
-    <Router>
-
-      <NavBar />
-      <Switch>
-        <Route path='/' exact/>
-      </Switch>
-    </Router>
-
-  </>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
